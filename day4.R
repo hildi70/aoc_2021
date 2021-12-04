06ea4f710f022ace843c7da9c318d66437442da2
hildi70 <karla.berger@web.de>
2021-12-04 19:20:41 +0100

    completed day4

--- day4.R
+++ day4.R
@@ -1,20 +1,28 @@
-#day4
+#part 1
+#Vector for drawn numbers
 numb <- scan('day4.txt', sep=",", nlines = 1)
+#Boards as list
 board <- read.table('day4.txt', skip =1)
 n_boards <- nrow(board)/5
 n_boards
 board$number <- rep(1:n_boards, each=5)
 boardlist <- split(board, board$number)
 boardlist <- lapply(boardlist, function(x) { x["number"] <- NULL; x })
+#Boards as logical (Danke Juli ;))
+boardlist_logical <- lapply(boardlist, `>`, -1)
 
+
+#Work in progress: I would like to express the second loop as lapply
 for (i in 1: length(numb)) {
   for (j in 1: length(boardlist)) {
     x <- boardlist[[j]][]!=numb[i]
-    boardlist[[j]][] <- boardlist[[j]][]*x
-    if (sum(colSums(boardlist[[j]]) == 0)>=1 || sum(rowSums(boardlist[[j]]) == 0)>=1) break;
+    boardlist_logical[[j]][] <- x*boardlist_logical[[j]][]
+    if (sum(colSums(boardlist_logical[[j]]) == 0)>=1 || sum(rowSums(boardlist_logical[[j]]) == 0)>=1) break;
   }
-  if (sum(colSums(boardlist[[j]]) == 0)>=1 || sum(rowSums(boardlist[[j]]) == 0)>=1) break;
+  if (sum(colSums(boardlist_logical[[j]]) == 0)>=1 || sum(rowSums(boardlist_logical[[j]]) == 0)>=1) break;
 }
 
-sum(boardlist[[j]])*numb[i] #54810 too high
 
+sum(boardlist[[j]]*boardlist_logical[[j]])*numb[i] #34506
+
+#part 2
