649d46f8ca7f9cc62ad5a3f24d960bdfbe3696b3
hildi70 <karla.berger@web.de>
2021-12-07 13:22:44 +0100

    completed day7

---
+++ day7.R
@@ -0,0 +1,16 @@
+#part 1
+crabs <- as.numeric(scan('day7.txt', sep=",", nlines = 1))
+
+fuel <- data.frame((matrix(nrow=1, ncol=length(crabs))))
+for (i in 0:max(crabs)) {
+  fuel[i,] <- unlist(sapply(crabs, function(x) {ifelse(x >=i, x-i, i-x)}))
+}
+
+min(rowSums(fuel)) # 347449
+
+#part 2
+fuel_new <- data.frame((matrix(nrow=1, ncol=length(fuel))))
+for (i in 1:nrow(fuel)) {
+  fuel_new[i,] <- unlist(sapply(fuel[i,], function(x) {max(cumsum(0:x))}))
+}
+min(rowSums(fuel_new)) #98039527
